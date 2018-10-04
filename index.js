95fdbb7d7913a8594d793ea712d3295de5051f79
Lim Chee Aun <cheeaun@gmail.com>
2018-10-05 00:17:58 +0800

    Prettify the addresses

--- index.js
+++ index.js
@@ -5,6 +5,27 @@ const spacetime = require('spacetime');
 
 const dateFormat = 'y-MM-ddTHH:mm:ss';
 
+// Only specialized for Singapore
+const prettyAddress = ({ name = '', address_1 = '', address_2 = '', address_3 = '', city = '' }) => {
+  const lowerName = name.toLowerCase();
+  if (address_1.toLowerCase().includes(name) || address_2.toLowerCase().includes(name) || address_3.toLowerCase().includes(name)) name = '';
+  let addr = [
+    name.trim().replace(/\s+,/, ''),
+    address_1.trim().replace(/\s+,/, ''),
+    address_2.trim().replace(/\s+,/, ''),
+    address_3.trim().replace(/\s+,/, ''),
+  ].filter(Boolean).join(', ');
+  if (city.trim() && !addr.toLowerCase().includes(city.toLowerCase())){
+    addr += ', ' + city;
+  }
+  if (!/singapore/i.test(addr)){
+    addr += ', Singapore';
+  } else if (/\d{5,}$/.test(addr) && !/singapore\s+\d{5,}$/.test(addr)){ // postcode at the end
+    addr += ' Singapore';
+  }
+  return addr;
+};
+
 module.exports = async (req, res) => {
   const now = spacetime.now('Asia/Singapore');
   const start_date_range = now.startOf('day').format(dateFormat);
@@ -46,6 +67,7 @@ module.exports = async (req, res) => {
           address_3,
           city,
           country,
+          pretty_address: prettyAddress(venue),
         },
         group: group.name,
         link,
